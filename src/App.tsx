89f7acb27d52d2bdd7ee8bfc828df2a4ec98b53e
Fake Dude <fakedude@gmail.com>
2024-12-23 03:00:36 +0400

    Refactor App component structure and enable fullscreen mode in main.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,3 @@
-// import { useState } from 'react';
-// import reactLogo from './assets/react.svg';
-// import twaLogo from './assets/tapps.png';
-// import viteLogo from '/vite.svg'
 import './App.css';
 
 import { TonConnectButton } from '@tonconnect/ui-react';
@@ -10,11 +6,11 @@ import WebApp from '@twa-dev/sdk';
 WebApp.ready();
 
 function App() {
-    return (
-        <div className="App">
-            <TonConnectButton />
-        </div>
-    );
+  return (
+    <div className="App">
+      <TonConnectButton />
+    </div>
+  );
 }
 
 export default App;
